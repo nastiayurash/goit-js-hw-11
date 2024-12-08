d5e24ce93f5a6e8febc7eafc97a99f486a21e771
Nastia Yurash <nasstyayurash@gmail.com>
2024-12-08 21:30:46 +0700

    remove loadmore button

--- src/main.js
+++ src/main.js
@@ -36,11 +36,11 @@ searchForm.addEventListener('submit', async (e) => {
     hideLoader();
     if (data.hits.length === 0) {
       iziToast.error({ message: 'Sorry, no images found. Please try again!' });
-      loadMoreButton.style.display = 'none';
+      
     } else {
       renderGallery(data.hits, galleryElement, true);
       page += 1;
-      loadMoreButton.style.display = 'block';
+      
     }
   } catch (error) {
     hideLoader();
@@ -48,25 +48,3 @@ searchForm.addEventListener('submit', async (e) => {
   }
 });
 
-loadMoreButton.addEventListener('click', async () => {
-  const query = searchForm.querySelector('input').value.trim();
-  showLoader();
-
-  try {
-    const data = await fetchImages(query, page);
-    hideLoader();
-    if (data && data.hits.length > 0) {
-      renderGallery(data.hits, galleryElement);
-      page += 1;
-    } else {
-      iziToast.info({ message: 'No more images available.' });
-      loadMoreButton.style.display = 'none';
-    }
-  } catch (error) {
-    hideLoader();
-    iziToast.error({ message: 'Error loading more images. Please try again!' });
-  }
-});
-
-
- 
